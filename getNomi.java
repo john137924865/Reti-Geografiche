37e28b9baeb148e4b0324bd6f5b307543686c6cd
john137924865 <96287823+john137924865@users.noreply.github.com>
2025-09-07 16:45:51 +0200

    .
    .

--- getNomi.java
+++ getNomi.java
@@ -3,6 +3,15 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 import java.io.File;
 import java.io.IOException;
 
@@ -12,8 +21,16 @@ public class getNomi {
             File input = new File("Hot 100 Artists.htm");
             Document doc = Jsoup.parse(input, "UTF-8");
 
-            // Seleziona tutti i nomi degli artisti
             Elements artists = doc.select("h3.c-title");
+
+            // Crea documento XML
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            org.w3c.dom.Document xmlDoc = dBuilder.newDocument();
+
+            // Root element <names>
+            org.w3c.dom.Element rootElement = xmlDoc.createElement("names");
+            xmlDoc.appendChild(rootElement);
 
             int count = 0;
             int skip = 2;
@@ -24,12 +41,28 @@ public class getNomi {
                 }
                 String name = artist.text().trim();
                 if (!name.isEmpty()) {
-                    System.out.println((count + 1) + ". " + name);
+                    // Crea elemento <name>
+                    org.w3c.dom.Element nameElement = xmlDoc.createElement("name");
+                    nameElement.setTextContent(name);
+                    rootElement.appendChild(nameElement);
+
                     count++;
-                    if (count >= 100) break; // solo i primi 100
+                    if (count >= 100) break;
                 }
             }
-        } catch (IOException e) {
+
+            // Scrivi il documento XML su file
+            TransformerFactory transformerFactory = TransformerFactory.newInstance();
+            Transformer transformer = transformerFactory.newTransformer();
+            transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+            transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "2");
+            DOMSource source = new DOMSource(xmlDoc);
+            StreamResult result = new StreamResult(new File("artists.xml"));
+            transformer.transform(source, result);
+
+            System.out.println("XML creato con successo!");
+
+        } catch (IOException | ParserConfigurationException | TransformerException e) {
             e.printStackTrace();
         }
     }
